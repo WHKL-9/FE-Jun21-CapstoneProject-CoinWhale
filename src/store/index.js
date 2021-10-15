abe0cefb7c6b5ccbf8d2f0c5eb9df498e22297da
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-10-15 16:28:58 +0200

    solved coindetails page bug

--- src/store/index.js
+++ src/store/index.js
@@ -63,10 +63,9 @@ const persistedReducer = persistReducer(persistConfig, bigReducer);
 export const configureStore = createStore(
   persistedReducer,
   initialState,
-  // process.env.REACT_APP_DEVELOPMENT
-  //   ? composeEnhancers(applyMiddleware(thunk))
-  //   : 
-  compose(applyMiddleware(thunk))
+  process.env.REACT_APP_DEVELOPMENT
+    ? composeEnhancers(applyMiddleware(thunk))
+    : compose(applyMiddleware(thunk))
 );
 
 // ! dont forget
