5be27841e569c0137df7497c2c26d96722085bf7
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-11-01 10:26:33 +0100

    updated footer with personal info

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -6,21 +6,40 @@ export default function MyFooter() {
   return (
     <Container fluid className="footerContainer my-0">
       <Row className="mb-0">
-          <Col xs={4} className="footerCol">
-              <div className="logoDiv d-flex mt-2">
-                <img src="https://res.cloudinary.com/demwthyen/image/upload/v1634156343/Coinwhale/SpoutingWhale_vxswmy.png" alt="Whale" className="logo" />
-              </div>
-              <p className="copyrightText">Copyright 2021 CoinWhale</p>
-          </Col>
-          <Col xs={4} className="footerCol">
-           <div className="text-center text-white"> 
-                  <p className="my-0">Contact Us</p>
-                  <p className="my-0">Imprint</p>
-                  <p className="my-0">Blog</p>
-           </div>
-          </Col>
-          <Col xs={4} className="footerCol">
-          </Col>
+        <Col xs={6} className="footerCol">
+          <div className="logoDiv d-flex mt-2">
+            <img
+              src="https://res.cloudinary.com/demwthyen/image/upload/v1634156343/Coinwhale/SpoutingWhale_vxswmy.png"
+              alt="Whale"
+              className="logo"
+            />
+          </div>
+          <p className="copyrightText">Built by WH in Berlin with coffee☕</p>
+        </Col>
+        <Col xs={6} className="footerCol">
+          <div className="text-right text-white mr-3">
+            <a
+              href="https://portfolio-henna-iota.vercel.app/"
+              target="_blank"
+              className="text-decoration-none text-white"
+            >
+              <p className="my-0">
+                <u>About WH</u>
+              </p>
+            </a>
+            <a
+              href="https://fe-jun21-m3-d10-netflix-solo-project.vercel.app/"
+              target="_blank"
+              className="text-decoration-none text-white"
+            >
+              <p className="my-0">
+                <u>Other Project</u>
+              </p>
+            </a>
+          </div>
+        </Col>
+        {/* <Col xs={4} className="footerCol">
+          </Col> */}
       </Row>
     </Container>
   );
